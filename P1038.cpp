a6c7dba6f5410e7cbd511fc0f37ed6fe92f186d2
qwerty250 <qiyue2001@gmail.com>
2021-05-12 00:51:48 +0800

    .

--- P1038.cpp
+++ P1038.cpp
@@ -1,84 +1,79 @@
-/*----------------------------------------------------------------
-MinCMaxF
-TLE. SPFA's fault?
-------------------------------------------------------------------*/
-#include <string.h>
 
-#include <algorithm>
-#include <climits>
-#include <iostream>
-#include <queue>
-#include <vector>
+// MCMF Dinic algorithm: https://blog.csdn.net/aiwo1376301646/article/details/104300253
+#include <bits/stdc++.h>
+
 #define rep(a, b, c) for (int a = b; a <= c; a++)
 #define per(a, b, c) for (int a = b; a >= c; a--)
-#define NEW_INDEX(y) y + 50;
+#define NEW_INDEX(y) y + n
 #define S_INDEX 0
-#define T_INDEX MAXN - 1
-typedef long long ll;
+#define T_INDEX n + m + 1
+
 using namespace std;
+const int maxn = 505, maxm = 1e5 + 1;
+int w[maxn][maxn];
+int n, m, s, t, tot = 1, head[maxn], dis[maxn], flow[maxn], pre[maxn],
+                last[maxn], maxflow, mincost;
+// dis最小花费;pre每个点的前驱；last每个点的所连的前一条边；flow源点到此处的流量
+bool vis[maxn];
+queue<int> q;
 
-int const MAXN = 105;
-int const MAXM = 505;
-int const INF = INT_MAX;
-int w[MAXN][MAXN];
-int lx[MAXN], ly[MAXN];
-int head[MAXN], cnt = 1;  // 这里特意把存图也贴出来，记住额外存一个参数c（费用）
-struct Edge {
-    int to, w, c, next;
-} edges[MAXM * 2];
-inline void add(int from, int to, int w, int c) {
-    edges[++cnt].to = to;
-    edges[cnt].w = w;
-    edges[cnt].c = c;
-    edges[cnt].next = head[from];
-    head[from] = cnt;
+struct edge {
+    int to, next, w, dis;  // w流量 dis花费
+} e[maxm];
+
+void add(int x, int y, int w, int dis) {
+    e[++tot].to = y;
+    e[tot].w = w;
+    e[tot].dis = dis;
+    e[tot].next = head[x];
+    head[x] = tot;
+
+    e[++tot].to = x;
+    e[tot].w = 0;
+    e[tot].dis = -dis;
+    e[tot].next = head[y];
+    head[y] = tot;
 }
-int n, m, s, t, last[MAXN], flow[MAXN], inq[MAXN], dis[MAXN];
-int maxflow, mincost;   // answers
 
-queue<int> Q;
-bool SPFA() {
-    while (!Q.empty()) Q.pop();
-    memset(last, -1, sizeof(last));
-    memset(inq, 0, sizeof(inq));
-    memset(dis, 127, sizeof(dis));
-    flow[s] = INF;
+bool spfa(int s, int t) {
+    memset(dis, 0x7f, sizeof(dis));
+    memset(flow, 0x7f, sizeof(flow));
+    memset(vis, 0, sizeof(vis));
+    q.push(s);
+    vis[s] = 1;
     dis[s] = 0;
-    Q.push(s);
-    while (!Q.empty()) {
-        int p = Q.front();
-        Q.pop();
-        inq[p] = 0;
-        for (int eg = head[p]; eg != 0; eg = edges[eg].next) {
-            int to = edges[eg].to, vol = edges[eg].w;
-            if (vol > 0 && dis[to] > dis[p] + edges[eg].c)  // 容量大于0才增广
+    pre[t] = -1;
+    while (!q.empty()) {
+        int now = q.front();
+        q.pop();
+        vis[now] = 0;
+        for (int i = head[now]; i != -1; i = e[i].next) {
+            int y = e[i].to;
+            if (e[i].w > 0 && dis[y] > dis[now] + e[i].dis)  //相当于求最短路
             {
-                last[to] = eg;                   // 记录上一条边
-                flow[to] = min(flow[p], vol);    // 更新下一个点的流量
-                dis[to] = dis[p] + edges[eg].c;  // 注意这里是c不是w
-                if (!inq[to]) {
-                    Q.push(to);
-                    inq[to] = 1;
+                dis[y] = dis[now] + e[i].dis;
+                pre[y] = now;
+                last[y] = i;
+                flow[y] = min(flow[now], e[i].w);
+                if (!vis[y]) {
+                    vis[y] = 1;
+                    q.push(y);
                 }
             }
         }
     }
-    return last[t] != -1;
+    return pre[t] != -1;
 }
 
-
-inline void MCMF()
-{
-    maxflow = mincost = 0;
-    while (SPFA()) {
-        maxflow += flow[t];
-        mincost += dis[t] * flow[t];
-        for (int i = t; i != s; i = edges[last[i] ^ 1].to) {
-            edges[last[i]].w -= flow[t];
-            edges[last[i] ^ 1].w += flow[t];
-            //cout<<"last[i]"<<last[i]<<"\t flow "<<edges[last[i]].w<<endl;
-        }
-        //cout<<"flow[t] = "<<flow[t]<<endl;
+void dfs() {
+    int now = t;
+    maxflow += flow[t];
+    mincost += flow[t] * dis[t];
+    while (now != s)  //从汇点一直回溯到源点
+    {
+        e[last[now]].w -= flow[t];
+        e[last[now] ^ 1].w += flow[t];
+        now = pre[now];
     }
 }
 
@@ -87,43 +82,31 @@ int main() {
     cin >> T;
     while (T--) {
         cin >> n >> m;
-        int x;  // cost of Girl i matching Boy j
-        rep(i, 1, m) {
-            rep(j, 1, n) {
-                cin >> x;
-                w[j][i] = x;
-            }
-        }
-        int k;  // # of girls liked by Boy i
 
-        rep(i, 1, n) {
-            rep(i, 1, m) ly[i] = 0;
-            cin >> k;
-            rep(j, 1, k) {
-                cin >> x;
-                ly[x] = 1;
-            }
-            rep(j, 1, m) {
-                if (ly[j]) {
-                    int ww = w[i][j];
-                    int t = NEW_INDEX(j);
-                    add(i, t, 1, ww);
-                    add(t, i, 0, -ww);
-                }
-            }
-        }
-        rep(i, 1, n) {
-            add(S_INDEX, i, 1, 0);
-            add(i, S_INDEX, 0, 0);
-        }
-        rep(j, 1, m) {
-            int t = NEW_INDEX(j);
-            add(t, T_INDEX, 1, 0);
-            add(T_INDEX, t, 0, 0);
-        }
         s = S_INDEX;
         t = T_INDEX;
-        MCMF();
+
+        rep(i, 1, m) {
+            rep(j, 1, n) { cin >> w[i][j]; }
+        }
+        rep(i, 1, n) {
+            int k;  // # of girls liked by Boy i
+            cin >> k;
+            rep(j, 1, k) {
+                int x;
+                cin >> x;
+                add(i, NEW_INDEX(x), 1, w[x][i]);
+            }
+        }
+        rep(i, 1, n) {
+            add(s, i, 1, 0);
+            // add(i, S_INDEX, 0, 0);
+        }
+        rep(j, 1, m) {
+            add(NEW_INDEX(j), t, 1, 0);
+            // add(T_INDEX, t, 0, 0);
+        }
+        while (spfa(s, t)) dfs();
         cout << maxflow << " " << mincost << endl;
     }
     return 0;
